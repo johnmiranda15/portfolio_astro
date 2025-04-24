bba5cb0b3b35c6f4241fec4332db8f4c8c0a77bc
Juan De Dios <mirandajohn634@gmail.com>
2025-04-24 09:37:03 -0500

    se agrega icons.astro

---
+++ src/const/const.js
@@ -0,0 +1,170 @@
+export const studies = [
+    {
+      title: "Técnico en Desarrollo de Software",
+      corporation: "UTP Panamá",
+      date: "2026",
+    },
+    {
+      title: "Curso Profesional de React JS",
+      corporation: "Udemy - Remoto",
+      date: "01/07/2025",
+    },
+    {
+      title: "Curso Profesional de JavaScript",
+      corporation: "Udemy - Remoto",
+      date: "22/11/2025",
+    },
+  ];
+  
+  export const IconKeys = {
+    html: "html",
+    css: "css",
+    javaScript: "javaScript",
+    react: "react",
+    astro: "astro",
+    nextjs: "nextjs",
+    tailwind: "tailwind",
+    figma: "figma",
+    reactQuery: "reactQuery",
+    typeScript: "typeScript",
+    zustand: "zustand",
+    git: "git",
+    github: "github",
+    stripe: "stripe",
+    shadcn: "shadcn",
+    nodejs: "nodejs",
+    mongodb: "mongodb",
+    vite: "vite",
+    // here add more icon keys
+  };
+  
+  export const skills = [
+    {
+      title: "Html",
+      icon: IconKeys.html,
+    },
+    {
+      title: "Css",
+      icon: IconKeys.css,
+    },
+    {
+      title: "JavaScript",
+      icon: IconKeys.javaScript,
+    },
+    {
+      title: "Astro",
+      icon: IconKeys.astro,
+    },
+    {
+      title: "Tailwindcss",
+      icon: IconKeys.tailwind,
+    },
+    {
+      title: "React Js",
+      icon: IconKeys.react,
+    },
+    {
+      title: "Next Js",
+      icon: IconKeys.nextjs,
+    },
+    {
+      title: "Git",
+      icon: IconKeys.git,
+    },
+  
+    {
+      title: "Github",
+      icon: IconKeys.github,
+    },
+    {
+      title: "Node Js",
+      icon: IconKeys.nodejs,
+    },
+    {
+      title: "TypeScript",
+      icon: IconKeys.typeScript,
+    },
+    {
+      title: "Figma",
+      icon: IconKeys.figma
+    },
+    {
+      title: "React Query",
+      icon: IconKeys.reactQuery,
+    },
+    {
+      title: "Vite",
+      icon: IconKeys.vite,
+    },
+    {
+      title: "Zustand",
+      icon: IconKeys.zustand,
+    },
+    {
+      title: "Mongo Db",
+      icon: IconKeys.mongodb,
+    },
+  ];
+  
+  export const projects = [
+    {
+      img: "../../../img/ecommerce-stripe-2.webp",
+      title: "Ecommerce Stripe",
+      link: "https://ecommerce-stripe-763m.vercel.app/",
+      description: `Proyecto FullStack de un ecommerce con pasarela de pagos stripe integrada, destalles de el product etc.  `,
+      technologies: [
+        {
+          icon: IconKeys.typeScript,
+        },
+        {
+          icon: IconKeys.nextjs,
+        },
+        {
+          icon: IconKeys.tailwind,
+        },
+        {
+          icon: IconKeys.mongodb,
+        },
+        {
+          icon: IconKeys.stripe,
+        },
+      ],
+    },
+    {
+      img: "../../../img/nexanime.webp",
+      title: "NexAnime",
+      link: "https://nexanime.vercel.app",
+      description: `plataforma de anime para ver los animes en tendencia , más valorados , estadísticas.`,
+      technologies: [
+        {
+          icon: IconKeys.typeScript,
+        },
+        {
+          icon: IconKeys.nextjs,
+        },
+        {
+          icon: IconKeys.tailwind,
+        },
+      ],
+    },
+    {
+      img: "../../../img/cyberpunk.webp",
+      title: "cyberpunk",
+      link: "https://preeminent-liger-266356.netlify.app/",
+      description: `este proyecto se trata de un clon de la pagína de cyberpunk , donde
+        el ususario puede ver los tailer de él juegos y también los
+        episodios de la serie....`,
+      technologies: [
+        {
+          icon: IconKeys.html,
+        },
+        {
+          icon: IconKeys.css,
+        },
+        {
+          icon: IconKeys.javaScript,
+        },
+      ],
+    },
+  ];
+  
